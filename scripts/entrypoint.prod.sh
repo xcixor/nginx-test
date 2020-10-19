75e8bf05d22447c3e1f00850dc3824e9a423e67e
xcixor <pndungu54@gmail.com>
2020-10-19 23:52:33 +0300

    fix(gunicorn): add debug log level and increase timeout

--- scripts/entrypoint.prod.sh
+++ scripts/entrypoint.prod.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 python manage.py collectstatic --no-input
 service nginx start
-gunicorn -b 0.0.0.0:8020 instaswap.wsgi:application --timeout 90
+gunicorn -b 0.0.0.0:8020 instaswap.wsgi:application --timeout 150 --log-level debug
 exec "$@"

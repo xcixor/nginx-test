9c28dab8abded5c626f574af6806bff8e37e9b35
xcixor <pndungu54@gmail.com>
2020-10-20 01:02:30 +0300

    restore cmd to start nginx in entry script

--- scripts/entrypoint.prod.sh
+++ scripts/entrypoint.prod.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
+service nginx start
 python manage.py collectstatic --no-input
-# service nginx start
 gunicorn -b 0.0.0.0:8020 instaswap.wsgi:application --timeout 150 --log-level debug
 exec "$@"

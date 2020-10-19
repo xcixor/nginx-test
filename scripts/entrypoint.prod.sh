3fb4074bf373534973908c95bfc9b794e13b8909
xcixor <pndungu54@gmail.com>
2020-10-20 02:24:03 +0300

    run server directly without gunicorn

--- scripts/entrypoint.prod.sh
+++ scripts/entrypoint.prod.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 python manage.py collectstatic --no-input
 service nginx start
-gunicorn -b 0.0.0.0:8020 instaswap.wsgi:application --timeout 150 --log-level debug
+# gunicorn -b 0.0.0.0:8020 instaswap.wsgi:application --timeout 150 --log-level debug
+python manage.py runserver 0.0.0.0:8020
 exec "$@"

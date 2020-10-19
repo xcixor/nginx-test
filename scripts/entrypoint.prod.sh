04defa265a70dec9c44e87cbddbf7e3952ec7e6b
xcixor <pndungu54@gmail.com>
2020-10-20 01:38:25 +0300

    start nginx after gunicorn

--- scripts/entrypoint.prod.sh
+++ scripts/entrypoint.prod.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-service nginx start
 python manage.py collectstatic --no-input
 gunicorn -b 0.0.0.0:8020 instaswap.wsgi:application --timeout 150 --log-level debug
+service nginx start
 exec "$@"

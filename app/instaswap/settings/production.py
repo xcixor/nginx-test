a9a6163963fe29ec585a747e871c57c86515f29a
xcixor <pndungu54@gmail.com>
2020-10-20 10:54:36 +0300

    collect static in a app folder

--- app/instaswap/settings/production.py
+++ app/instaswap/settings/production.py
@@ -1,6 +1,8 @@
 from .base import *
 DEBUG = False
 
-STATIC_ROOT = STATIC_ROOT = '/var/www/html/static/'
+STATIC_ROOT = '/home/app/instaswap/frontend/static/'
+# STATIC_ROOT = '/var/www/html/static/'
+
 
 
